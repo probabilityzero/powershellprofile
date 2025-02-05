2b2b68e5f6bafe83532c3c04a9e4effdd2a00197
Prime <primecortex@outlook.com>
2025-02-05 10:38:07 +0600

    ​

---
+++ Utilities/More.ps1
@@ -0,0 +1,5 @@
+
+function bolt {
+    Set-Location  "E:\Windows\Apps\bolt.diy"
+    pnpm run dev
+}
--- profile.ps1
+++ profile.ps1
@@ -1,19 +1,13 @@
 oh-my-posh init pwsh --config "$env:PWSH_DIR\themes\synth.omp.json" | Invoke-Expression
 
-function bolt {
-    cd  "E:\Windows\Apps\bolt.diy"
-    pnpm run dev
-}
-
-
 . "$env:PWSH_DIR\Shortcuts.ps1"
+. "$env:PWSH_DIR\Utilities\Navigation.ps1"
 
-# Utilities
 . "$env:PWSH_DIR\Utilities\Git.ps1"
 . "$env:PWSH_DIR\Utilities\Scoop.ps1"
 . "$env:PWSH_DIR\Utilities\Anaconda.ps1"
 . "$env:PWSH_DIR\Utilities\WSL.ps1"
 
-. "$env:PWSH_DIR\Utilities\Navigation.ps1"
+. "$env:PWSH_DIR\Utilities\More.ps1"
 
 . "$env:PWSH_DIR\Modules\Chololatey.ps1"
